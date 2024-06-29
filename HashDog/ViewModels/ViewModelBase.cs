1479bbcf52b0ee89c16668d57d25a84f4478769d
Zyldzkie <88105329+Zyldzkie@users.noreply.github.com>
2024-06-29 23:02:21 +0800

    init avalonia files

---
+++ HashDog/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace HashDog.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ HashDog/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace HashDog.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
